04c1f8fe71431ad5be112e54b001b23b65aea6b8
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-08 23:28:21 +0200

    Add snapper-gui-git

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -9,4 +9,4 @@ rustup default stable
 makepkg -si
 cd || exit
 rm -rf ./git
-paru --noprogressbar --noconfirm -S waterfox-g4-bin sweet-kde-git paper-icon-theme snap-pac-grub
+paru --noprogressbar --noconfirm -S waterfox-g4-bin sweet-kde-git paper-icon-theme snap-pac-grub snapper-gui-git
