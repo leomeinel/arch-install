5f4acff758629219e8e1be81c197b5ccd5e6d808
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-09 23:31:06 +0200

    Add ungoogled-chromium and replace waterfox

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -10,4 +10,4 @@ rustup default stable
 makepkg -si
 cd
 rm -rf ./git
-paru --noprogressbar --noconfirm -S waterfox-g4-bin sweet-kde-git paper-icon-theme snap-pac-grub snapper-gui-git
+paru --noprogressbar --noconfirm -S librewolf ungoogled-chromium sweet-kde-git papirus-icon-theme snap-pac-grub snapper-gui-git
