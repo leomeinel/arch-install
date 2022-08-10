318d57c475d5026a0f319fd9bf78aacefb1aa3f8
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-10 14:35:22 +0200

    umount only specified disks (test)

--- partition-disks.sh
+++ partition-disks.sh
@@ -4,10 +4,11 @@ DISK1="vda"
 DISK2="vdb"
 KEYMAP="de-latin1"
 
-umount -a
 set -e
 loadkeys "$KEYMAP"
 timedatectl set-ntp true
+umount /dev/"$DISK1"
+umount /dev/"$DISK2"
 sgdisk -Z /dev/"$DISK1"
 sgdisk -Z /dev/"$DISK2"
 sgdisk -o /dev/"$DISK1"
