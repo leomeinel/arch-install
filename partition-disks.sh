ca9ad91bfb258297ba5f92397cc97ef8a0677301
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-10 15:13:32 +0200

    Inform kernel about partition changes

--- partition-disks.sh
+++ partition-disks.sh
@@ -14,6 +14,7 @@ sgdisk -n 0:0:+1G -t 1:ef00 /dev/"$DISK1"
 sgdisk -n 0:0:+1G -t 1:ef00 /dev/"$DISK2"
 sgdisk -n 0:0:0 -t 1:fd00 /dev/"$DISK1"
 sgdisk -n 0:0:0 -t 1:fd00 /dev/"$DISK2"
+partprobe
 mkfs.fat -n BOOT -F32 /dev/"$DISK1"1
 mkfs.fat -n BOOT -F32 /dev/"$DISK2"1
 mdadm --create --verbose --level=1 --metadata=1.2 --raid-devices=2 /dev/md/md0 /dev/"$DISK1"2 /dev/"$DISK2"2
