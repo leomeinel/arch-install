438125cf6fbcaa8e5a1351b1d174a80cc7185b88
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 15:41:29 +0200

    Update sysuser-setup.sh

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -10,7 +10,3 @@ rustup default stable
 makepkg -si --noprogressbar --noconfirm --needed
 cd
 rm -rf ./git
-sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
-paru -S --noprogressbar --noconfirm --needed librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
-paru -Syu --noprogressbar --noconfirm
-paru -Scc --noprogressbar --noconfirm
