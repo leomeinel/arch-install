c9357dc2f81ef03cfabb0b216aa03883ca27e2b8
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 15:44:06 +0200

    Set time

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 set -e
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
 sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
 paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
 paru -Syu
