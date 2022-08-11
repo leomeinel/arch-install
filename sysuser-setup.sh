edb70697d8f20ff8cacee5974ae615f294b8d111
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 11:23:30 +0200

    Fix spacing

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -11,5 +11,5 @@ makepkg -si
 cd
 rm -rf ./git
 paru -S --noprogressbar --noconfirm librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub  pacman-log-orphans-hook snapper-gui-git
-paru -Syu--noprogressbar --noconfirm
+paru -Syu --noprogressbar --noconfirm
 paru -Scc --noprogressbar --noconfirm
