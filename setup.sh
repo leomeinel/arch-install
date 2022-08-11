5b3c5f1bd23a3c88430b57b52f2e865f4fe7b275
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 14:51:36 +0200

    debug (test)

--- setup.sh
+++ setup.sh
@@ -27,18 +27,13 @@ passwd "$HOMEUSER"
 sed -i 's/#Color/Color/;s/#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
 {
   echo ""
-  echo "# Include local repo: aur"
-  echo "[options]"
-  echo "Include = /etc/repo_aur.conf"
-} > /etc/pacman.conf
-{
   echo "[options]"
   echo "CacheDir = /var/lib/repo/aur"
   echo ""
   echo "[aur]"
   echo "SigLevel = PackageOptional DatabaseOptional"
   echo "Server = file:///var/lib/repo/aur"
-} > /etc/repo_aur.conf
+} > /etc/pacman.conf
 pacman -S --noprogressbar --noconfirm --needed plasma-desktop plasma-wayland-session kgpg dolphin gwenview kalendar kmail kompare okular print-manager spectacle plasma-pa bleachbit sddm sddm-kcm plasma-nm neofetch htop mpv libreoffice-still rxvt-unicode zram-generator virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber rustup grub grub-btrfs efibootmgr mtools inetutils bluez bluez-utils cups hplip alsa-utils openssh rsync reflector acpi acpi_call tlp openbsd-netcat nss-mdns acpid ntfs-3g nvidia-settings notepadqq intellij-idea-community-edition jdk17-openjdk jdk-openjdk jdk11-openjdk mariadb screen gradle arch-audit ark noto-fonts snapper lrzip lzop p7zip unarchiver unrar devtools
 umount /.snapshots
 rm -rf /.snapshots
