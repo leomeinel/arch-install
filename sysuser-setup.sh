82afcfec84dffa725e5c0e89474181ce29d16656
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 13:45:43 +0200

    Correct include

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -11,7 +11,7 @@ makepkg -si
 cd
 rm -rf ./git
 sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
-sudo sed -i 's/#CacheDir/CacheDir/;s/#ParallelDownloads = 5\nInclude = /etc/paru-chroot.conf/ParallelDownloads = 10/' /etc/pacman.conf
+sudo sed -i 's/#CacheDir/CacheDir/;s/#ParallelDownloads = 5/ParallelDownloads = 10\nInclude = \/etc\/paru-chroot.conf/' /etc/pacman.conf
 paru -S --noprogressbar --noconfirm librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
 paru -Syu --noprogressbar --noconfirm
 paru -Scc --noprogressbar --noconfirm
