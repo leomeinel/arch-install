72176cb781bb95c7dfd04e921d6fa4dfde5d71ba
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 15:43:11 +0200

    Remove some flags

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,6 @@
 
 set -e
 sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
-paru -S --noprogressbar --noconfirm --needed librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
-paru -Syu --noprogressbar --noconfirm
-paru -Scc --noprogressbar --noconfirm
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
+paru -Syu
+paru -Scc
