7cc041aef5e5090cb94e93c68930d6925b96f4e8
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 13:55:54 +0200

    Add --needed to paru

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -12,6 +12,6 @@ cd
 rm -rf ./git
 sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
 sudo sed -i 's/#CacheDir/CacheDir/;s/#ParallelDownloads = 5/ParallelDownloads = 10\nInclude = \/etc\/paru-chroot.conf/' /etc/pacman.conf
-paru -S --noprogressbar --noconfirm librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
+paru -S --noprogressbar --noconfirm --needed librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
 paru -Syu --noprogressbar --noconfirm
 paru -Scc --noprogressbar --noconfirm
