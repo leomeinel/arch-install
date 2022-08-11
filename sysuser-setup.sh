f085ec423e38d82ed79ee430b9b0925c202cb477
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 15:54:32 +0200

    Add post-install.sh

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -2,11 +2,14 @@
 
 set -e
 cd
-mkdir ./git
-cd ./git
+mkdir ~/git
+cd ~/git
 git clone https://aur.archlinux.org/paru.git
-cd ./paru
+cd ~/git/paru
 rustup default stable
 makepkg -si --noprogressbar --noconfirm --needed
-cd
-rm -rf ./git
+cd ~/git
+git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git
+mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
+rm -rf ~/git
+chmod +x ~/post-install.sh
