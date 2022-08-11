874339b0cb62fd2b53c0e331fe2004210f75e690
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-11 12:09:44 +0200

    Fix spacing

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -14,6 +14,6 @@ sudo sed -i 's/#LocalRepo/LocalRepo/' /etc/paru.conf
 sudo sed -i 's/#Chroot/Chroot/' /etc/paru.conf
 sudo sed -i 's/#RemoveMake/RemoveMake/' /etc/paru.conf
 sudo sed -i 's/#CleanAfter/CleanAfter/' /etc/paru.conf
-paru -S --noprogressbar --noconfirm librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub  pacman-log-orphans-hook snapper-gui-git
+paru -S --noprogressbar --noconfirm librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
 paru -Syu --noprogressbar --noconfirm
 paru -Scc --noprogressbar --noconfirm
