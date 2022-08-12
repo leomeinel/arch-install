82fdc6fdca26ac0fcc4fb95b1e91986d0eec181d
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-12 23:38:14 +0200

    Replace sweet with sweet full

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,6 @@
 set -e
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git sweet-kde-theme-git
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git sweet-theme-full-git
 paru -Scc
 paru -Syu
