59453c1f29d4bf65f014a45b7e5b4060630aea9b
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-12 15:28:41 +0200

    Do not install tuxedo-control-center-bin
    The Software seems Like bloat and any benefits can be achieved through other software.
    The build also seems broken

--- post-install.sh
+++ post-install.sh
@@ -6,6 +6,4 @@ sudo hwclock --systohc
 sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
 paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store sweet-kde-theme-git papirus-icon-theme snap-pac-grub pacman-log-orphans-hook snapper-gui-git
 paru -Scc
-paru -S --needed tuxedo-control-center-bin
-paru -Scc
 paru -Syu
