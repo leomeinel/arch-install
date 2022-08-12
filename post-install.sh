dba4081d60d944b24c6e7583213a66c32cd9c666
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 01:49:20 +0200

    Add multicolor-sddm-theme

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,6 @@
 set -e
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped solarized-sddm-theme otf-raleway
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme
 paru -Scc
 paru -Syu
