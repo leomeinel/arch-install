9e90adabd66a6dbcb26c152631e15e849d8473a6
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 01:04:13 +0200

    Update themes

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,6 @@
 set -e
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde kvantum-theme-arc
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git arc-gtk-theme papirus-icon-theme-stripped
 paru -Scc
 paru -Syu
