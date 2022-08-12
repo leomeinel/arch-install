a91d9e5921a34feddedcd4e257597947716785a8
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-12 17:41:31 +0200

    Move editing paru.conf

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -12,3 +12,4 @@ git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
 rm -rf ~/git
 chmod +x ~/post-install.sh
+sudo sed -i 's/#LocalRepo/LocalRepo/;s/#Chroot/Chroot/;s/#RemoveMake/RemoveMake/;s/#CleanAfter/CleanAfter/' /etc/paru.conf
