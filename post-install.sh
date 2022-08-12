056148f4beb415e2aa94ff443efef48fba87a860
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-12 23:03:51 +0200

    Add sweet kde

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,6 @@
 set -e
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git sweet-kde-theme-git
 paru -Scc
 paru -Syu
