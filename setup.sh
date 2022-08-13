e167923163bd6c20baab01c2964b04822696342e
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 14:06:40 +0200

    Add Germany for more consistent mirrors

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ VIRTUSER="virt"
 HOMEUSER="leo"
 TIMEZONE="Europe/Amsterdam"
 DOMAIN="meinel.dev"
-MIRRORCOUNTRIES="Netherlands"
+MIRRORCOUNTRIES="Netherlands,Germany"
 GRUBRESOLUTION="2560x1440"
 
 set -e
