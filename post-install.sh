e25d6d16070b66569832d1b9ef30474b090200a0
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 12:39:24 +0200

    Remove Post-Install.sh when done

--- post-install.sh
+++ post-install.sh
@@ -6,3 +6,4 @@ sudo hwclock --systohc
 paru -S --needed --fm vim librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
 paru -Scc
 paru -Syu
+rm -rf ~/post-install.sh
