ca66afa9812b489cb74a0d974f9a8d162bcc2c0f
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 13:23:21 +0200

    Move localectl

--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+KEYMAP="de-latin1"
+KEYLAYOUT="de"
+
 set -e
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
+sudo localectl set-keymap "$KEYMAP"
+sudo localectl set-x11-keymap "$KEYLAYOUT"
 paru -S --needed --fm vim librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
 paru -Scc
 paru -Syu
