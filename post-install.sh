5a678ad84b12c25b87f118215c9e8b66349a7a77
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 02:58:48 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,6 @@
 set -e
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
-paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
+paru -S --needed --fm vim librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
 paru -Scc
 paru -Syu
