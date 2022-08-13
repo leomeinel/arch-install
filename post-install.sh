454158044505608d2b613d6585554b4892b11398
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-13 22:02:27 +0200

    Remove filemanager

--- post-install.sh
+++ post-install.sh
@@ -8,7 +8,7 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 sudo localectl set-keymap "$KEYMAP"
 sudo localectl set-x11-keymap "$KEYLAYOUT"
-paru -S --needed --fm vim librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
 paru -Scc
 paru -Syu
 rm -rf ~/post-install.sh
