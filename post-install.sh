7e4e3c470c355c6f45d17e1122eff59147ecf276
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-14 03:55:38 +0200

    Add packages

--- post-install.sh
+++ post-install.sh
@@ -8,7 +8,8 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 sudo localectl set-keymap "$KEYMAP"
 sudo localectl set-x11-keymap "$KEYLAYOUT"
-paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway
+paru -S --needed librewolf-bin ungoogled-chromium chromium-extension-web-store snap-pac-grub pacman-log-orphans-hook snapper-gui-git arc-kde-git papirus-icon-theme-stripped multicolor-sddm-theme otf-raleway hal laptop-mode-tools 
 paru -Scc
 paru -Syu
+sudo systemctl enable laptop-mode.service
 rm -rf ~/post-install.sh
