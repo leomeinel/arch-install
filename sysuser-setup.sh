084ad83a60d3756eee420c36563f1fd3d4e48651
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-15 15:51:08 +0200

    Add comments

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+# Fail on error
 set -e
+
+# Install paru
 mkdir ~/git
 cd ~/git
 git clone https://aur.archlinux.org/paru.git
 cd ~/git/paru
 rustup default stable
 makepkg -si --noprogressbar --noconfirm --needed
+
+# Set up post-install.sh
 cd ~/git
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
-rm -rf ~/git
 chmod +x ~/post-install.sh
+
+# Remove repo
+rm -rf ~/git
