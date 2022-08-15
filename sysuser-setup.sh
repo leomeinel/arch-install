0ed7069f4ef654f8334d2c5e126b8915b494f57c
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-15 22:11:42 +0200

    Change git clone

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -4,16 +4,13 @@
 set -e
 
 # Install paru
-mkdir ~/git
-cd ~/git
-git clone https://aur.archlinux.org/paru.git
+git clone https://aur.archlinux.org/paru.git ~/git/paru
 cd ~/git/paru
 rustup default stable
-makepkg -si --noprogressbar --noconfirm --needed
+makepkg -sri --noprogressbar --noconfirm --needed
 
 # Set up post-install.sh
-cd ~/git
-git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git
+git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
 chmod +x ~/post-install.sh
 
