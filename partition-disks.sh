7b6579603d58a1c6018b0116635a4005ceee3145
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-15 15:59:56 +0200

    Add comments

--- partition-disks.sh
+++ partition-disks.sh
@@ -4,7 +4,11 @@ KEYMAP="de-latin1"
 MIRRORCOUNTRIES="NL,DE,DK,FR"
 
 umount -AR /mnt
+
+# Fail on error
 set -e
+
+# Detect disks
 SIZE1="$(lsblk -rno TYPE,SIZE | grep "disk" | sed 's/disk//' | sed -n '1p' | tr -d "[:space:]")"
 SIZE2="$(lsblk -rno TYPE,SIZE | grep "disk" | sed 's/disk//' | sed -n '2p' | tr -d "[:space:]")"
 if [ "$SIZE1" = "$SIZE2" ]
@@ -15,6 +19,8 @@ else
   echo "ERROR: There are not exactly 2 disks with the same size attached!"
   exit 19
 fi
+
+# Prompt user
 read -rp "Erase $DISK1 and $DISK2? (Type 'yes' in capital letters): " choice
 case "$choice" in
   YES ) echo "Erasing $DISK1 and $DISK2..."
@@ -23,6 +29,8 @@ case "$choice" in
   exit 125
   ;;
 esac
+
+# Detect and erase old crypt and raid1 volumes
 if lsblk -rno TYPE | grep -q "crypt"
 then
   cryptsetup luksClose "$(lsblk -Mrno TYPE,NAME | grep "crypt" | sed 's/crypt//' | tr -d "[:space:]")"
@@ -41,8 +49,12 @@ then
     mdadm --zero-superblock "$DISK1"2
     mdadm --zero-superblock "$DISK2"2
 fi
+
+# Load $KEYMAP and set time
 loadkeys "$KEYMAP"
 timedatectl set-ntp true
+
+# Erase and partition disks
 sgdisk -Z "$DISK1"
 sgdisk -Z "$DISK2"
 sgdisk -n 0:0:+1G -t 1:ef00 "$DISK1"
@@ -51,11 +63,17 @@ sgdisk -n 0:0:0 -t 1:fd00 "$DISK1"
 sgdisk -n 0:0:0 -t 1:fd00 "$DISK2"
 mkfs.fat -n BOOT -F32 "$DISK1"1
 mkfs.fat -n BOOT -F32 "$DISK2"1
+
+# Configure raid1
 mdadm --create --verbose --level=1 --metadata=1.2 --raid-devices=2 --homehost=any /dev/md/md0 "$DISK1"2 "$DISK2"2
+
+# Configure encryption
 cryptsetup open --type plain -d /dev/urandom /dev/md/md0 to_be_wiped
 cryptsetup close to_be_wiped
 cryptsetup -y -v -h sha512 -s 512 luksFormat /dev/md/md0
 cryptsetup luksOpen /dev/md/md0 md0_crypt
+
+# Configure btrfs
 mkfs.btrfs -L MDCRYPT /dev/mapper/md0_crypt
 mount /dev/mapper/md0_crypt /mnt
 btrfs subvolume create /mnt/@
@@ -63,6 +81,8 @@ btrfs subvolume create /mnt/@var
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@tmp
 btrfs subvolume create /mnt/@snapshots
+
+# Mount volumes
 umount /mnt
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=256 /dev/mapper/md0_crypt /mnt
 mkdir /mnt/var
@@ -75,6 +95,8 @@ mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=258 /de
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=259 /dev/mapper/md0_crypt /mnt/tmp
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=260 /dev/mapper/md0_crypt /mnt/.snapshots
 mount "$DISK1"1 /mnt/boot
+
+# Install packages
 {
   echo "base"
   echo "base-devel"
@@ -123,7 +145,11 @@ then
   } >> /root/packages.txt
 fi
 pacstrap /mnt - < /root/packages.txt
+
+# Configure /mnt/etc/fstab
 genfstab -U /mnt >> /mnt/etc/fstab
+
+# Prepare /mnt/git/mdadm-encrypted-btrfs/setup.sh
 mkdir /mnt/git
 cd /mnt/git
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git
