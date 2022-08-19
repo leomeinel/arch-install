6648087cff964bffffb43b04acfe3a55c4c43e5d
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-20 00:23:08 +0200

    Remove TERM

--- setup.sh
+++ setup.sh
@@ -21,7 +21,6 @@ set -e
   echo "DISK2P1_PARTUUID=\"$(blkid -t LABEL="BOOT" -s PARTUUID -o value | sed -n '2p' | tr -d "[:space:]")\""
   echo "DISK2P2_PARTUUID=\"$(blkid -t LABEL="any:md0" -s PARTUUID -o value | sed -n '2p' | tr -d "[:space:]")\""
   echo "EDITOR=\"/usr/bin/vim\""
-  echo "TERM=\"/usr/bin/alacritty\""
   echo "BROWSER=\"/usr/bin/chromium\""
 } >> /etc/environment
 
