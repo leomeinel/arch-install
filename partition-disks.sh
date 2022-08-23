fd89e2e57e91c7814006f3ebf87da103c65c2664
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-23 02:19:23 +0200

    Remove debug statements [Fixes #13]
    Apparently I'm too stupid to debug, but at least not too stupid to fix a bug just by debugging :)
    Seems okay to me 
    This finally fixes #13

--- partition-disks.sh
+++ partition-disks.sh
@@ -58,18 +58,18 @@ then
   OLD_RAID="$(lsblk -Mrnpo TYPE,NAME | grep "raid1" | sed 's/raid1//' | tr -d "[:space:]")"
   if cryptsetup isLuks "$OLD_RAID"
   then
-    cryptsetup erase "$OLD_RAID" || { echo 'DEBUG: cryptsetup erase "$OLD_RAID" - 0' && exit; }
-    partprobe "$DISK1" || { echo 'DEBUG: partprobe "$DISK1" - 0' && exit; }
-    partprobe "$DISK2" || { echo 'DEBUG: partprobe "$DISK2" - 0' && exit; }
+    cryptsetup erase "$OLD_RAID"
+    partprobe "$DISK1"
+    partprobe "$DISK2"
   fi
-  sgdisk -Z "$OLD_RAID" || { echo 'DEBUG: sgdisk -Z "$OLD_RAID" - 0' && exit; }
-  mdadm --stop --scan || { echo 'DEBUG: mdadm --stop --scan - 0' && exit; }
-  partprobe "$DISK1" || { echo 'DEBUG: partprobe "$DISK1" - 1' && exit; }
-  partprobe "$DISK2" || { echo 'DEBUG: partprobe "$DISK2" - 1' && exit; }
-  mdadm --zero-superblock "$DISK1P2" || { echo 'DEBUG: mdadm --zero-superblock "$DISK1P2" - 0' && exit; }
-  mdadm --zero-superblock "$DISK2P2" || { echo 'DEBUG: mdadm --zero-superblock "$DISK2P2" - 0' && exit; }
-  partprobe "$DISK1" || { echo 'DEBUG: partprobe "$DISK1" - 2' && exit; }
-  partprobe "$DISK2" || { echo 'DEBUG: partprobe "$DISK2" - 2' && exit; }
+  sgdisk -Z "$OLD_RAID"
+  mdadm --stop --scan
+  partprobe "$DISK1"
+  partprobe "$DISK2"
+  mdadm --zero-superblock "$DISK1P2"
+  mdadm --zero-superblock "$DISK2P2"
+  partprobe "$DISK1"
+  partprobe "$DISK2"
 fi
 
 # Load $KEYMAP and set time
