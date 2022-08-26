199aaa5de4e805b2c31c7d4e94a442391f646860
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 16:44:55 +0200

    Update custom-bootbackup.sh

--- pacman/hooks/scripts/custom-bootbackup.sh
+++ pacman/hooks/scripts/custom-bootbackup.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
 
+/usr/bin/rsync -a --delete /.boot.bak/* /.boot.bak.old/
+/usr/bin/rsync -a --delete /boot/* /.boot.bak/
+/usr/bin/umount /boot
+/usr/bin/mount PARTUUID="$DISK2P1_PARTUUID" /boot/
+/usr/bin/rsync -a --delete /.boot.bak/* /boot/
+/usr/bin/umount /boot
+/usr/bin/mount PARTUUID="$DISK1P1_PARTUUID" /boot
