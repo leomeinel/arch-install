58046cc7208333e22c008dcfe5dff7a301eef829
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 23:46:55 +0200

    Move makepkg

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -3,17 +3,6 @@
 # Fail on error
 set -e
 
-# Install opendoas-sudo
-git clone https://aur.archlinux.org/opendoas-sudo.git ~/git/opendoas-sudo
-cd ~/git/opendoas-sudo
-makepkg -sri --noprogressbar --noconfirm --needed
-
-# Install paru
-git clone https://aur.archlinux.org/paru.git ~/git/paru
-cd ~/git/paru
-rustup default stable
-makepkg -sri --noprogressbar --noconfirm --needed
-
 # Set up post-install.sh
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
