2b724b8d2f2995df94dd0eb8bd77854fe3bf7205
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 21:10:29 +0200

    Replace sudo with doas

--- post-install.sh
+++ post-install.sh
@@ -7,11 +7,11 @@ KEYLAYOUT="de"
 set -e
 
 # Configure clock
-sudo timedatectl set-ntp true
+doas timedatectl set-ntp true
 
 # Configure $KEYMAP
-sudo localectl set-keymap "$KEYMAP"
-sudo localectl set-x11-keymap "$KEYLAYOUT"
+doas localectl set-keymap "$KEYMAP"
+doas localectl set-x11-keymap "$KEYLAYOUT"
 
 # Install packages
 paru -S --needed - < ~/packages_post-install.txt
@@ -39,171 +39,171 @@ paru -Scc
 ## ipv4
 
 ### Flush and delete all chains
-sudo iptables -F
-sudo iptables -X
+doas iptables -F
+doas iptables -X
 
 ### Set up new chains
-sudo iptables -L | grep -q "Chain INPUT" ||
-sudo iptables -N INPUT
-sudo iptables -L | grep -q "Chain FORWARD" ||
-sudo iptables -N FORWARD
-sudo iptables -L | grep -q "Chain OUTPUT" ||
-sudo iptables -N OUTPUT
+doas iptables -L | grep -q "Chain INPUT" ||
+doas iptables -N INPUT
+doas iptables -L | grep -q "Chain FORWARD" ||
+doas iptables -N FORWARD
+doas iptables -L | grep -q "Chain OUTPUT" ||
+doas iptables -N OUTPUT
 
 ### Allow all connections on all chains to start
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
+doas iptables -P INPUT ACCEPT
+doas iptables -P FORWARD ACCEPT
+doas iptables -P OUTPUT ACCEPT
 
 ### ACCEPT LOOPBACK
-sudo iptables -A INPUT -i lo -j ACCEPT
+doas iptables -A INPUT -i lo -j ACCEPT
 
 ### FIRST PACKET HAS TO BE TCP SYN
-sudo iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+doas iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 
 ### DROP ALL INVALID PACKETS
-sudo iptables -A INPUT -m state --state INVALID -j DROP
-sudo iptables -A FORWARD -m state --state INVALID -j DROP
-sudo iptables -A OUTPUT -m state --state INVALID -j DROP
+doas iptables -A INPUT -m state --state INVALID -j DROP
+doas iptables -A FORWARD -m state --state INVALID -j DROP
+doas iptables -A OUTPUT -m state --state INVALID -j DROP
 
 ### Block packets with bogus TCP flags
-sudo iptables -A INPUT -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-sudo iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-sudo iptables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-sudo iptables -A INPUT -p tcp --tcp-flags FIN,ACK FIN -j DROP
-sudo iptables -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
-sudo iptables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags FIN,ACK FIN -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
 
 ### Drop NULL packets
-sudo iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 
 ### DROP XMAS PACKETS
-sudo iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 
 ### DROP EXCESSIVE TCP RST PACKETS
-sudo iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
-sudo iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+doas iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+doas iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
 
 ### DROP SYN-FLOOD PACKETS
-sudo iptables -A INPUT -p tcp -m state --state NEW -m limit --limit 2/second --limit-burst 2 -j ACCEPT
-sudo iptables -A INPUT -p tcp -m state --state NEW -j DROP
+doas iptables -A INPUT -p tcp -m state --state NEW -m limit --limit 2/second --limit-burst 2 -j ACCEPT
+doas iptables -A INPUT -p tcp -m state --state NEW -j DROP
 
 ### Drop fragments
-sudo iptables -A INPUT -f -j DROP
-sudo iptables -A FORWARD -f -j DROP
-sudo iptables -A OUTPUT -f -j DROP
+doas iptables -A INPUT -f -j DROP
+doas iptables -A FORWARD -f -j DROP
+doas iptables -A OUTPUT -f -j DROP
 
 ### Drop SYN packets with suspicious MSS value
-sudo iptables -A INPUT -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP
+doas iptables -A INPUT -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP
 
 ### Block spoofed packets
-sudo iptables -A INPUT -s 127.0.0.0/8 ! -i lo -j DROP
+doas iptables -A INPUT -s 127.0.0.0/8 ! -i lo -j DROP
 
 ### Drop ICMP
-sudo iptables -A INPUT -p icmp -j DROP
+doas iptables -A INPUT -p icmp -j DROP
 
 ### Allow default ktorrent ports (Forard them if not using UPnP)
-sudo iptables -A INPUT -p tcp --dport 6881 -j ACCEPT
-sudo iptables -A INPUT -p udp --dport 7881 -j ACCEPT
-sudo iptables -A INPUT -p udp --dport 8881 -j ACCEPT
+doas iptables -A INPUT -p tcp --dport 6881 -j ACCEPT
+doas iptables -A INPUT -p udp --dport 7881 -j ACCEPT
+doas iptables -A INPUT -p udp --dport 8881 -j ACCEPT
 
 ### ALLOW ESTABLISHED CONNECTIONS
-sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+doas iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 ### Set default policies for chains
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
+doas iptables -P INPUT DROP
+doas iptables -P FORWARD ACCEPT
+doas iptables -P OUTPUT ACCEPT
 
 ## ipv6
 
 ### Flush and delete all chains
-sudo ip6tables -F
-sudo ip6tables -X
+doas ip6tables -F
+doas ip6tables -X
 
 ### Set up new chains
-sudo ip6tables -L | grep -q "Chain INPUT" ||
-sudo ip6tables -N INPUT
-sudo ip6tables -L | grep -q "Chain INPUT" ||
-sudo ip6tables -N FORWARD
-sudo ip6tables -L | grep -q "Chain INPUT" ||
-sudo ip6tables -N OUTPUT
+doas ip6tables -L | grep -q "Chain INPUT" ||
+doas ip6tables -N INPUT
+doas ip6tables -L | grep -q "Chain INPUT" ||
+doas ip6tables -N FORWARD
+doas ip6tables -L | grep -q "Chain INPUT" ||
+doas ip6tables -N OUTPUT
 
 ### Allow all connections on all chains to start
-sudo ip6tables -P INPUT ACCEPT
-sudo ip6tables -P FORWARD ACCEPT
-sudo ip6tables -P OUTPUT ACCEPT
+doas ip6tables -P INPUT ACCEPT
+doas ip6tables -P FORWARD ACCEPT
+doas ip6tables -P OUTPUT ACCEPT
 
 ### ACCEPT LOOPBACK
-sudo ip6tables -A INPUT -i lo -j ACCEPT
+doas ip6tables -A INPUT -i lo -j ACCEPT
 
 ### FIRST PACKET HAS TO BE TCP SYN
-sudo ip6tables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+doas ip6tables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 
 ### DROP ALL INVALID PACKETS
-sudo ip6tables -A INPUT -m state --state INVALID -j DROP
-sudo ip6tables -A FORWARD -m state --state INVALID -j DROP
-sudo ip6tables -A OUTPUT -m state --state INVALID -j DROP
+doas ip6tables -A INPUT -m state --state INVALID -j DROP
+doas ip6tables -A FORWARD -m state --state INVALID -j DROP
+doas ip6tables -A OUTPUT -m state --state INVALID -j DROP
 
 ### Block packets with bogus TCP flags
-sudo ip6tables -A INPUT -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
-sudo ip6tables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
-sudo ip6tables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
-sudo ip6tables -A INPUT -p tcp --tcp-flags FIN,ACK FIN -j DROP
-sudo ip6tables -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
-sudo ip6tables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags FIN,ACK FIN -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags ACK,URG URG -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -j DROP
 
 ### Drop NULL packets
-sudo ip6tables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 
 ### DROP XMAS PACKETS
-sudo ip6tables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 
 ### DROP EXCESSIVE TCP RST PACKETS
-sudo ip6tables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
-sudo ip6tables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+doas ip6tables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+doas ip6tables -A INPUT -p tcp --tcp-flags RST RST -j DROP
 
 ### DROP SYN-FLOOD PACKETS
-sudo ip6tables -A INPUT -p tcp -m state --state NEW -m limit --limit 2/second --limit-burst 2 -j ACCEPT
-sudo ip6tables -A INPUT -p tcp -m state --state NEW -j DROP
+doas ip6tables -A INPUT -p tcp -m state --state NEW -m limit --limit 2/second --limit-burst 2 -j ACCEPT
+doas ip6tables -A INPUT -p tcp -m state --state NEW -j DROP
 
 ### Drop fragments
-sudo ip6tables -A INPUT -m frag -j DROP
-sudo ip6tables -A FORWARD -m frag -j DROP
-sudo ip6tables -A OUTPUT -m frag -j DROP
+doas ip6tables -A INPUT -m frag -j DROP
+doas ip6tables -A FORWARD -m frag -j DROP
+doas ip6tables -A OUTPUT -m frag -j DROP
 
 ### Drop SYN packets with suspicious MSS value
-sudo ip6tables -A INPUT -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP
+doas ip6tables -A INPUT -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP
 
 ### Block spoofed packets
-sudo ip6tables -A INPUT -s ::1/128 ! -i lo -j DROP
+doas ip6tables -A INPUT -s ::1/128 ! -i lo -j DROP
 
 ### Drop ICMP
-sudo ip6tables -A INPUT -p icmp -j DROP
+doas ip6tables -A INPUT -p icmp -j DROP
 
 ### Allow default ktorrent ports (Forard them if not using UPnP)
-sudo ip6tables -A INPUT -p tcp --dport 6881 -j ACCEPT
-sudo ip6tables -A INPUT -p udp --dport 7881 -j ACCEPT
-sudo ip6tables -A INPUT -p udp --dport 8881 -j ACCEPT
+doas ip6tables -A INPUT -p tcp --dport 6881 -j ACCEPT
+doas ip6tables -A INPUT -p udp --dport 7881 -j ACCEPT
+doas ip6tables -A INPUT -p udp --dport 8881 -j ACCEPT
 
 ### ALLOW ESTABLISHED CONNECTIONS
-sudo ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+doas ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 ### Set default policies for chains
-sudo ip6tables -P INPUT DROP
-sudo ip6tables -P FORWARD ACCEPT
-sudo ip6tables -P OUTPUT ACCEPT
+doas ip6tables -P INPUT DROP
+doas ip6tables -P FORWARD ACCEPT
+doas ip6tables -P OUTPUT ACCEPT
 
 ### Save rules to /etc/iptables
-sudo sh -c 'iptables-save > /etc/iptables/iptables.rules'
-sudo sh -c 'ip6tables-save > /etc/iptables/ip6tables.rules'
-sudo chmod 644 /etc/iptables/*.rules
+doas sh -c 'iptables-save > /etc/iptables/iptables.rules'
+doas sh -c 'ip6tables-save > /etc/iptables/ip6tables.rules'
+doas chmod 644 /etc/iptables/*.rules
 
 # Enable systemd services
-sudo systemctl enable laptop-mode.service
-sudo systemctl enable sddm
-sudo systemctl enable iptables
-sudo systemctl enable ip6tables
+doas systemctl enable laptop-mode.service
+doas systemctl enable sddm
+doas systemctl enable iptables
+doas systemctl enable ip6tables
 
 # Generate nvidia config if needed
 pacman -Qq "nvidia-settings" &&
@@ -212,4 +212,4 @@ rm -f ~/nvidia-install.sh
 
 # Remove script
 rm -f ~/post-install.sh
-sudo rm -f ~/packages_post-install.txt
+doas rm -f ~/packages_post-install.txt
