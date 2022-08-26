a0b686e3388656edaecaf29a2aa4875383e79568
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 18:00:06 +0200

    Use ~/packages_post-install.txt

--- post-install.sh
+++ post-install.sh
@@ -14,18 +14,7 @@ sudo localectl set-keymap "$KEYMAP"
 sudo localectl set-x11-keymap "$KEYLAYOUT"
 
 # Install packages
-{
-  echo "librewolf-bin"
-  echo "chromium-extension-web-store"
-  echo "snap-pac-grub"
-  echo "snapper-gui-git"
-  echo "laptop-mode-tools"
-  echo "macchina"
-  echo "arc-kde-git"
-  echo "papirus-icon-theme-stripped"
-  echo "sddm-nordic-theme-git"
-} > ~/packages.txt
-paru --needed - < ~/packages.txt
+paru --needed - < ~/packages_post-install.txt
 paru -Scc
 
 # Configure iptables
@@ -222,4 +211,4 @@ rm -f ~/nvidia-install.sh
 
 # Remove script
 rm -f ~/post-install.sh
-rm -f ~/packages.txt
+rm -f ~/packages_post-install.txt
