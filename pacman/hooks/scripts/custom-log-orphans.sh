19acd27f12e5791a54744b9e701a1415bfcb9505
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 16:46:57 +0200

    Update custom-log-orphans.sh

--- pacman/hooks/scripts/custom-log-orphans.sh
+++ pacman/hooks/scripts/custom-log-orphans.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
 
+PKGS="$(/usr/bin/pacman -Qtdq)"
+if [ -n "$PKGS" ]
+then
+  {
+    /usr/bin/echo "The following packages are installed but not required (anymore): "
+    /usr/bin/echo "$PKGS"
+    /usr/bin/echo "You can remove them all using 'pacman -Qtdq | pacman -Rns -'"
+  }
+fi
