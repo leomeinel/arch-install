33e172d86f5270d07403a2a3dcc892e53df56cd6
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 21:53:04 +0200

    Remove doas

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -12,7 +12,6 @@ makepkg -sri --noprogressbar --noconfirm --needed
 # Set up post-install.sh
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
-doas mv /packages_post-install.txt ~/packages_post-install.txt
 chmod +x ~/post-install.sh
 
 # Remove repo
