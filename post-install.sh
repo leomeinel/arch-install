7fd283c12390fc20430c07fc9bc755a1c7dfc5fd
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-26 18:55:42 +0200

    Fix paru

--- post-install.sh
+++ post-install.sh
@@ -14,8 +14,7 @@ sudo localectl set-keymap "$KEYMAP"
 sudo localectl set-x11-keymap "$KEYLAYOUT"
 
 # Install packages
-sudo chown $USER ~/packages_post-install.txt
-paru --needed - < ~/packages_post-install.txt
+paru -S --needed - < ~/packages_post-install.txt
 paru -Scc
 
 # Configure iptables
