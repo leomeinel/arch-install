d818f41426ef6e99f1d99d151e47ff869c3e5754
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-27 05:12:31 +0200

    Add some commands to sudo

--- setup.sh
+++ setup.sh
@@ -124,7 +124,8 @@ chmod +x /git/mdadm-encrypted-btrfs/sysuser-setup.sh
 
 ## sudo
 echo "%sudo ALL=(ALL:ALL) /usr/bin/mkarchroot" > /etc/sudoers.d/sudo
-echo "%sudo ALL=(ALL:ALL) /usr/bin/makechrootpkg" >> /etc/sudoers.d/sudo
+echo "%sudo ALL=(ALL:ALL) SETENV: /usr/bin/makechrootpkg" >> /etc/sudoers.d/sudo
+echo "%sudo ALL=(ALL:ALL) /usr/bin/arch-nspawn" >> /etc/sudoers.d/sudo
 
 ## opendoas
 mv /git/mdadm-encrypted-btrfs/etc/doas.conf /etc/
