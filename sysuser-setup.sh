f7a3c142a0f19b6cc358b33cf5aea8719f768271
Leopold Johannes Meinel <leo@meinel.dev>
2022-08-27 06:22:52 +0200

    Move packages_post-install.txt

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -6,6 +6,7 @@ set -e
 # Set up post-install.sh
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/post-install.sh
+mv ~/git/mdadm-encrypted-btrfs/packages_post-install.txt ~/packages_post-install.txt
 chmod +x ~/post-install.sh
 
 # Remove repo
