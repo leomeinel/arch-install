12f7c218f04517830a707ca2b39ca50c71118779
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-02 22:28:48 +0200

    Use this branch

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Set up post-install.sh
-git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
+git clone --branch encrypted-boot https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/
 mv ~/git/mdadm-encrypted-btrfs/packages_post-install.txt ~/
 chmod +x ~/post-install.sh
