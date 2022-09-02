e8d6f9d05e7bed596003dcf2a24819c63431ab8e
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-02 19:16:20 +0200

    Maybe fix erasing old crypt volumes

--- partition-disks.sh
+++ partition-disks.sh
@@ -51,17 +51,25 @@ case "$choice" in
 esac
 
 # Detect and erase old crypt volumes
-## TODO: Encrypted /boot
 if lsblk -rno TYPE | grep -q "crypt"
 then
-  OLD_CRYPT="$(lsblk -Mrno TYPE,NAME | grep "crypt" | sed 's/crypt//' | tr -d "[:space:]")"
-  DISK1P2="$(lsblk -rnpo NAME "$DISK1" | sed -n '3p' | tr -d "[:space:]")"
-  DISK2P2="$(lsblk -rnpo NAME "$DISK2" | sed -n '3p' | tr -d "[:space:]")"
-  cryptsetup luksClose "$OLD_CRYPT"
+  OLD_CRYPT_0="$(lsblk -Mrno TYPE,NAME | grep "crypt" | sed 's/crypt//' | sed -n '1p' | tr -d "[:space:]")"
+  OLD_CRYPT_1="$(lsblk -Mrno TYPE,NAME | grep "crypt" | sed 's/crypt//' | sed -n '2p' | tr -d "[:space:]")"
+  OLD_CRYPT_2="$(lsblk -Mrno TYPE,NAME | grep "crypt" | sed 's/crypt//' | sed -n '3p' | tr -d "[:space:]")"
+  cryptsetup luksClose "$OLD_CRYPT_0"
+  cryptsetup luksClose "$OLD_CRYPT_1"
+  cryptsetup luksClose "$OLD_CRYPT_2"
+  OLD_BOOTCRYPT_0="$(lsblk -rnpo NAME | grep -B1 '/dev/mapper/boot_crypt$' | sed -n '1p' | tr -d '[:space:]')"
+  OLD_BOOTCRYPT_1="$(lsblk -rnpo NAME | grep -B1 '/dev/mapper/boot_crypt_backup$' | sed -n '1p' | tr -d '[:space:]')"
+  OLD_RAID="$(lsblk -Mrnpo TYPE,NAME | grep "raid1" | sed 's/raid1//' | tr -d "[:space:]")"
+  cryptsetup erase "$OLD_BOOTCRYPT_0"
+  cryptsetup erase "$OLD_BOOTCRYPT_1"
+  cryptsetup erase "$OLD_RAID"
   if lsblk -rno TYPE | grep -q "raid1"
   then
+    DISK1P2="$(lsblk -rnpo NAME "$DISK1" | sed -n '3p' | tr -d "[:space:]")"
+    DISK2P2="$(lsblk -rnpo NAME "$DISK2" | sed -n '3p' | tr -d "[:space:]")"
     OLD_RAID="$(lsblk -Mrnpo TYPE,NAME | grep "raid1" | sed 's/raid1//' | tr -d "[:space:]")"
-    cryptsetup erase "$OLD_RAID"
     sgdisk -Z "$OLD_RAID"
     mdadm --stop --scan
     mdadm --zero-superblock "$DISK1P2"
@@ -70,12 +78,21 @@ then
 fi
 
 # Detect and erase closed crypt and raid1 volumes
-## TODO: Encrypted /boot
 if lsblk -rno TYPE | grep -q "raid1"
 then
   DISK1P2="$(lsblk -rnpo NAME "$DISK1" | sed -n '3p' | tr -d "[:space:]")"
   DISK2P2="$(lsblk -rnpo NAME "$DISK2" | sed -n '3p' | tr -d "[:space:]")"
+  OLD_BOOTCRYPT_0="$(lsblk -rnpo NAME | grep -B1 '/dev/mapper/boot_crypt$' | sed -n '1p' | tr -d '[:space:]')"
+  OLD_BOOTCRYPT_1="$(lsblk -rnpo NAME | grep -B1 '/dev/mapper/boot_crypt_backup$' | sed -n '1p' | tr -d '[:space:]')"
   OLD_RAID="$(lsblk -Mrnpo TYPE,NAME | grep "raid1" | sed 's/raid1//' | tr -d "[:space:]")"
+  if cryptsetup isLuks "$OLD_BOOTCRYPT_0"
+  then
+    cryptsetup erase "$OLD_BOOTCRYPT_0"
+  fi
+  if cryptsetup isLuks "$OLD_BOOTCRYPT_1"
+  then
+    cryptsetup erase "$OLD_BOOTCRYPT_1"
+  fi
   if cryptsetup isLuks "$OLD_RAID"
   then
     cryptsetup erase "$OLD_RAID"
