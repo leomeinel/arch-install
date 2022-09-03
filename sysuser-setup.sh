aff21379d022fd12682039538e35b2648988d967
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-03 12:43:19 +0200

    Install bdf-unifont

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -3,11 +3,19 @@
 # Fail on error
 set -e
 
+# Install bdf-unifont (make dependency for grub-improved-luks2-git) beforehand
+git clone https://aur.archlinux.org/bdf-unifont.git ~/git/bdf-unifont
+cd ~/git/bdf-unifont
+makepkg -sri --noprogressbar --noconfirm --needed
+
 # Install grub-improved-luks2-git
 git clone https://aur.archlinux.org/grub-improved-luks2-git.git ~/git/grub-improved-luks2-git
 cd ~/git/grub-improved-luks2-git
 makepkg -sri --noprogressbar --noconfirm --needed
 sudo pacman -Syu grub-btrfs
+
+# Remove bdf-unifont (make dependency for grub-improved-luks2-git)
+sudo pacman -Rsnc bdf-unifont
 
 # Set up post-install.sh
 git clone --branch encrypted-boot https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
