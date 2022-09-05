e84b01d89b774e302934098efd223531fa3851eb
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-05 17:12:12 +0200

    Change to main branch

--- partition-disks.sh
+++ partition-disks.sh
@@ -189,7 +189,7 @@ pacstrap /mnt - < /root/mdadm-encrypted-btrfs/packages_partition-disks.txt
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Prepare /mnt/git/mdadm-encrypted-btrfs/setup.sh
-git clone --branch encrypted-boot-partition https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git /mnt/git/mdadm-encrypted-btrfs
+git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git /mnt/git/mdadm-encrypted-btrfs
 chmod +x /mnt/git/mdadm-encrypted-btrfs/setup.sh
 
 # Remove repo
