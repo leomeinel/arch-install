ddce3c96700347795d103cc975a0ae813d8d77ae
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-11 22:25:47 +0200

    Add vscodium-bin-marketplace
    This should enable more extensions (mainly because the rust-anlyzer from default extensions doesn't work).
    It means that there won't only be free extensions but there is still a choice!

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -8,6 +8,11 @@ git clone https://aur.archlinux.org/vscodium-bin.git ~/git/vscodium-bin
 cd ~/git/vscodium-bin
 makepkg -sri --noprogressbar --noconfirm --needed
 
+# Install vscodium-bin-marketplace
+git clone https://aur.archlinux.org/vscodium-bin-marketplace.git ~/git/vscodium-bin-marketplace
+cd ~/git/vscodium-bin-marketplace
+makepkg -sri --noprogressbar --noconfirm --needed
+
 # Set up post-install.sh
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/
