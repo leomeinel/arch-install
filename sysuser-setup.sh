64c4702b2258984dca124e43ae7febd7a6cf257f
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-11 18:08:10 +0200

    Install vscodium-bin

--- sysuser-setup.sh
+++ sysuser-setup.sh
@@ -3,6 +3,11 @@
 # Fail on error
 set -e
 
+# Install vscodium-bin
+git clone https://aur.archlinux.org/vscodium-bin.git ~/git/vscodium-bin
+cd ~/git/vscodium-bin
+makepkg -sri --noprogressbar --noconfirm --needed
+
 # Set up post-install.sh
 git clone https://github.com/LeoMeinel/mdadm-encrypted-btrfs.git ~/git/mdadm-encrypted-btrfs
 mv ~/git/mdadm-encrypted-btrfs/post-install.sh ~/
