f7256ddb3807f8097ad53d7c54981df21b5810b0
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-16 00:20:52 +0200

    Add upgrade grub hook

---
+++ etc/pacman.d/hooks/scripts/custom-upgrade-grub.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###
+# File: custom-upgrade-grub.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2022 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+
+/usr/local/bin/grub-install
