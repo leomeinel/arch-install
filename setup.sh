3980bc6bd33eaadebbcbe577f1c261da8654de43
Leopold Johannes Meinel <leo@meinel.dev>
2022-10-13 17:47:27 +0200

    Big restructure
    This is a more logical approach to the configuration when reading through the script

--- setup.sh
+++ setup.sh
@@ -45,7 +45,8 @@ passwd "$HOMEUSER"
 echo "Enter password for $GUESTUSER"
 passwd "$GUESTUSER"
 
-# Configure /etc/pacman.conf , /etc/makepkg.conf and /etc/xdg/reflector/reflector.conf
+# Setup pacman
+## Configure /etc/pacman.conf , /etc/makepkg.conf and /etc/xdg/reflector/reflector.conf
 {
     echo "--save /etc/pacman.d/mirrorlist"
     echo "--country $MIRRORCOUNTRIES"
@@ -58,14 +59,12 @@ chmod 644 /etc/xdg/reflector/reflector.conf
 sed -i 's/^#PACMAN_AUTH=.*/PACMAN_AUTH=(doas)/' /etc/makepkg.conf
 sed -i 's/^#Color/Color/;s/^#ParallelDownloads =.*/ParallelDownloads = 10/;s/^#CacheDir/CacheDir/' /etc/pacman.conf
 pacman-key --init
-
-# Update mirrors
+## Update mirrors
 reflector --save /etc/pacman.d/mirrorlist --country $MIRRORCOUNTRIES --protocol https --latest 20 --sort rate
-
-# Install packages
+## Install packages
 pacman -Syu --noprogressbar --noconfirm --needed - </git/arch-install/pkgs-setup.txt
 
-# rsync /git/arch-install/etc/ to /etc
+# Setup /etc
 rsync -rq /git/arch-install/etc/ /etc
 ## opendoas
 chown -c root:root /etc/doas.conf
@@ -145,35 +144,117 @@ chmod 644 /etc/systemd/system/snapper-cleanup.timer.d/override.conf
 ## However those scripts use different scripts/commands so it is very hard to tell which should actually be allowed.
 ## FUTURE GOAL: REPLACE sudo WITH doas
 echo "%sudo ALL=(ALL:ALL) NOPASSWD:ALL" >/etc/sudoers.d/sudo
-## Set up post.sh
+## Run sysuser.sh
 chmod +x /git/arch-install/sysuser.sh
 su -c '/git/arch-install/sysuser.sh '"$SYSUSER $VIRTUSER $HOMEUSER $GUESTUSER"'' "$SYSUSER"
 echo "%sudo ALL=(ALL:ALL) ALL" >/etc/sudoers.d/sudo
 
-# Configure /etc/crypttab
+# Configure /etc
+## Configure /etc/crypttab
 MD0UUID="$(blkid -s UUID -o value /dev/md/md0)"
 MD1UUID="$(blkid -s UUID -o value /dev/md/md1)"
 {
     echo "md0_crypt UUID=$MD0UUID /etc/luks/keys/md0_crypt.key luks,key-slot=1"
     echo "md1_crypt UUID=$MD1UUID none luks,key-slot=0"
 } >/etc/crypttab
+## Configure /etc/localtime, /etc/locale.conf, /etc/vconsole.conf, /etc/hostname and /etc/hosts
+ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+hwclock --systohc
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/;s/^#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/;s/^#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/;s/^#nl_NL.UTF-8 UTF-8/nl_NL.UTF-8 UTF-8/' /etc/locale.gen
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+locale-gen
+echo "KEYMAP=$KEYMAP" >/etc/vconsole.conf
+echo "$HOSTNAME" >/etc/hostname
+{
+    echo "127.0.0.1  localhost"
+    echo "127.0.1.1  $HOSTNAME.$DOMAIN	$HOSTNAME"
+    echo "::1  ip6-localhost ip6-loopback"
+    echo "ff02::1  ip6-allnodes"
+    echo "ff02::2  ip6-allrouters"
+} >/etc/hosts
+## Configure /etc/fwupd/uefi_capsule.conf
+{
+    echo ""
+    echo "# Custom"
+    echo "## Set /efi as mountpoint"
+    echo "OverrideESPMountPoint=/efi"
+} >>/etc/fwupd/uefi_capsule.conf
+## Configure /etc/cryptboot.conf
+git clone https://github.com/LeoMeinel/cryptboot.git /git/cryptboot
+mv /git/cryptboot/cryptboot.conf /etc/
+chmod 644 /etc/cryptboot.conf
+## Configure /etc/ssh/sshd_config
+{
+    echo ""
+    echo "# Override"
+    echo "PasswordAuthentication no"
+    echo "AuthenticationMethods publickey"
+    echo "PermitRootLogin no"
+} >>/etc/ssh/sshd_config
+## Configure /etc/xdg/user-dirs.defaults
+sed -i 's/^TEMPLATES=.*/TEMPLATES=Documents\/Templates/;s/^PUBLICSHARE=.*/PUBLICSHARE=Documents\/Public/;s/^DESKTOP=.*/DESKTOP=Documents\/Desktop/;s/^MUSIC=.*/MUSIC=Documents\/Music/;s/^PICTURES=.*/PICTURES=Documents\/Pictures/;s/^VIDEOS=.*/VIDEOS=Documents\/Videos/' /etc/xdg/user-dirs.defaults
+## Configure /etc/mdadm.conf
+mdadm --detail --scan >>/etc/mdadm.conf
+## Configure /etc/usbguard/usbguard-daemon.conf and /etc/usbguard/rules.conf
+usbguard generate-policy >/etc/usbguard/rules.conf
+usbguard add-user -g usbguard --devices=modify,list,listen --policy=list --exceptions=listen
+## Configure /etc/pam.d/system-login, /etc/security/faillock.conf, /etc/pam.d/su and /etc/pam.d/su-l
+echo "auth optional pam_faildelay.so delay=8000000" >>/etc/pam.d/system-login
+sed -i 's/^#.*dir.*=.*/dir = \/var\/lib\/faillock/' /etc/security/faillock.conf
+echo "auth required pam_wheel.so use_uid" >>/etc/pam.d/su
+echo "auth required pam_wheel.so use_uid" >>/etc/pam.d/su-l
+## Configure /etc/audit/auditd.conf
+sed -i 's/^log_group.*=.*/log_group = audit/' /etc/audit/auditd.conf
+## mDNS
+### Configure mDNS in /etc/systemd/resolved.conf
+sed -i 's/^#MulticastDNS=.*/MulticastDNS=no/' /etc/systemd/resolved.conf
+### Configure mDNS in /etc/nsswitch.conf
+sed -i 's/^hosts: mymachines/hosts: mymachines mdns_minimal [NOTFOUND=return]/' /etc/nsswitch.conf
+## Configure /etc/luks/keys
+mkdir -p /etc/luks/keys
+dd bs=1024 count=4 if=/dev/urandom of=/etc/luks/keys/md0_crypt.key iflag=fullblock
+chmod 000 /etc/luks/keys/md0_crypt.key
+echo "Enter passphrase for /dev/md/md0"
+cryptsetup -v luksAddKey /dev/disk/by-uuid/"$MD0UUID" /etc/luks/keys/md0_crypt.key
+## Configure /etc/mkinitcpio.conf
+sed -i 's/^FILES=.*/FILES=(\/etc\/luks\/keys\/md0_crypt.key)/;s/^MODULES=.*/MODULES=(btrfs)/;s/^HOOKS=.*/HOOKS=(base udev autodetect keyboard keymap consolefont modconf block mdadm_udev encrypt filesystems fsck)/' /etc/mkinitcpio.conf
+### If on nvidia add nvidia nvidia_modeset nvidia_uvm nvidia_drm
+pacman -Qq "nvidia-dkms" &&
+    sed -i '/^MODULES=.*/s/)$/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+## Configure /etc/default/grub
+sed -i "s/^#GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/;s/^#GRUB_TERMINAL_OUTPUT=.*/GRUB_TERMINAL_OUTPUT=\"gfxterm\"/;s/^GRUB_GFXPAYLOAD_LINUX=.*/GRUB_GFXPAYLOAD_LINUX=keep/;s/^GRUB_GFXMODE=.*/GRUB_GFXMODE=""$GRUBRESOLUTION""x32,auto/;s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet loglevel=3 audit=1 lsm=landlock,lockdown,yama,integrity,apparmor,bpf iommu=pt zswap.enabled=0 cryptdevice=UUID=$MD0UUID:md0_crypt cryptkey=rootfs:\/etc\/luks\/keys\/md0_crypt.key cryptdevice=UUID=$MD1UUID:md1_crypt root=\/dev\/mapper\/md1_crypt\"/;s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"quiet loglevel=3 audit=1 lsm=landlock,lockdown,yama,integrity,apparmor,bpf iommu=pt zswap.enabled=0 cryptdevice=UUID=$MD0UUID:md0_crypt cryptkey=rootfs:\/etc\/luks\/keys\/md0_crypt.key cryptdevice=UUID=$MD1UUID:md1_crypt root=\/dev\/mapper\/md1_crypt\"/;s/^#GRUB_DISABLE_SUBMENU=.*/GRUB_DISABLE_SUBMENU=y/;s/^GRUB_DEFAULT=.*/GRUB_DEFAULT=0/;s/^#GRUB_SAVEDEFAULT=.*/GRUB_SAVEDEFAULT=false/" /etc/default/grub
+### If on nvidia add nvidia_drm.modeset=1 and set linux as default kernel
+pacman -Qq "nvidia-dkms" &&
+    sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=.*/s/"$/ nvidia_drm.modeset=1"/;/^GRUB_CMDLINE_LINUX=.*/s/"$/ nvidia_drm.modeset=1"/' /etc/default/grub
+pacman -Qq "intel-ucode" &&
+    sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=.*/s/"$/ intel_iommu=on"/;/^GRUB_CMDLINE_LINUX=.*/s/"$/ intel_iommu=on"/' /etc/default/grub
 
-# Set default java
-archlinux-java set java-17-openjdk
+# Setup /usr
+rsync -rq /git/arch-install/usr/ /usr
+mv /git/cryptboot/grub-install /usr/local/bin/
+mv /git/cryptboot/cryptboot /usr/local/bin/
+mv /git/cryptboot/cryptboot-efikeys /usr/local/bin/
+## Configure /usr/share/gruvbox/gruvbox.yml
+chmod 755 /usr/share/gruvbox
+chmod 644 /usr/share/gruvbox/gruvbox.yml
+## Configure symlinks
+chmod 755 /usr/local/bin/cryptboot
+chmod 755 /usr/local/bin/cryptboot-efikeys
+chmod 755 /usr/local/bin/grub-install
+ln -s "$(which nvim)" /usr/local/bin/edit
+ln -s "$(which nvim)" /usr/local/bin/vedit
+ln -s "$(which nvim)" /usr/local/bin/vi
+ln -s "$(which nvim)" /usr/local/bin/vim
+chmod 755 /usr/local/bin/ex
+chmod 755 /usr/local/bin/view
+chmod 755 /usr/local/bin/vimdiff
+chmod 755 /usr/local/bin/edit
+chmod 755 /usr/local/bin/vedit
+chmod 755 /usr/local/bin/vi
+chmod 755 /usr/local/bin/vim
 
-# Add wallpapers to /usr/share/wallpapers/Custom/content
-mkdir -p /usr/share/wallpapers/Custom/content
-git clone https://github.com/LeoMeinel/wallpapers.git /git/wallpapers
-mv /git/wallpapers/*.jpg /git/wallpapers/*.png /usr/share/wallpapers/Custom/content/
-chmod -R 755 /usr/share/wallpapers/Custom
-chmod 644 /usr/share/wallpapers/Custom/content/*
-
-# Add screenshot folder to /usr/share/screenshots/
-mkdir /share/screenshots
-chmod 1775 /share/screenshots
-chown -R :share /share
-
-# Configure /usr/share/snapper/config-templates/default and add snapper configs
+# Configure /usr
+## Configure /usr/share/snapper/config-templates/default and add snapper configs
 umount /.snapshots
 rm -rf /.snapshots
 cp /usr/share/snapper/config-templates/default /usr/share/snapper/config-templates/root
@@ -221,93 +302,25 @@ chown :sudo /home/.snapshots
 chmod 750 /share/.snapshots
 chmod a+rx /share/.snapshots
 chown :sudo /share/.snapshots
+## Add wallpapers to /usr/share/wallpapers/Custom/content
+mkdir -p /usr/share/wallpapers/Custom/content
+git clone https://github.com/LeoMeinel/wallpapers.git /git/wallpapers
+mv /git/wallpapers/*.jpg /git/wallpapers/*.png /usr/share/wallpapers/Custom/content/
+chmod -R 755 /usr/share/wallpapers/Custom
+chmod 644 /usr/share/wallpapers/Custom/content/*
 
-# Configure /etc/fwupd/uefi_capsule.conf
-{
-    echo ""
-    echo "# Custom"
-    echo "## Set /efi as mountpoint"
-    echo "OverrideESPMountPoint=/efi"
-} >>/etc/fwupd/uefi_capsule.conf
+# Configure /var
+## Configure /var/games
+chown :games /var/games
 
-# Configure /etc/cryptboot.conf
-git clone https://github.com/LeoMeinel/cryptboot.git /git/cryptboot
-mv /git/cryptboot/cryptboot.conf /etc/
-chmod 644 /etc/cryptboot.conf
+# Configure /share
+# Add screenshot folder to /share/screenshots
+mkdir /share/screenshots
+chmod 1775 /share/screenshots
+chown -R :share /share
 
-# Configure /etc/ssh/sshd_config
-{
-    echo ""
-    echo "# Override"
-    echo "PasswordAuthentication no"
-    echo "AuthenticationMethods publickey"
-    echo "PermitRootLogin no"
-} >>/etc/ssh/sshd_config
-
-# Configure /usr
-rsync -rq /git/arch-install/usr/ /usr
-mv /git/cryptboot/grub-install /usr/local/bin/
-mv /git/cryptboot/cryptboot /usr/local/bin/
-mv /git/cryptboot/cryptboot-efikeys /usr/local/bin/
-## Configure /usr/share/gruvbox/gruvbox.yml
-chmod 755 /usr/share/gruvbox
-chmod 644 /usr/share/gruvbox/gruvbox.yml
-## Configure symlinks
-chmod 755 /usr/local/bin/cryptboot
-chmod 755 /usr/local/bin/cryptboot-efikeys
-chmod 755 /usr/local/bin/grub-install
-ln -s "$(which nvim)" /usr/local/bin/edit
-ln -s "$(which nvim)" /usr/local/bin/vedit
-ln -s "$(which nvim)" /usr/local/bin/vi
-ln -s "$(which nvim)" /usr/local/bin/vim
-chmod 755 /usr/local/bin/ex
-chmod 755 /usr/local/bin/view
-chmod 755 /usr/local/bin/vimdiff
-chmod 755 /usr/local/bin/edit
-chmod 755 /usr/local/bin/vedit
-chmod 755 /usr/local/bin/vi
-chmod 755 /usr/local/bin/vim
-
-# Configure /etc/localtime, /etc/locale.conf, /etc/vconsole.conf, /etc/hostname and /etc/hosts
-ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
-hwclock --systohc
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/;s/^#de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/;s/^#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/;s/^#nl_NL.UTF-8 UTF-8/nl_NL.UTF-8 UTF-8/' /etc/locale.gen
-echo "LANG=en_US.UTF-8" >/etc/locale.conf
-locale-gen
-echo "KEYMAP=$KEYMAP" >/etc/vconsole.conf
-echo "$HOSTNAME" >/etc/hostname
-{
-    echo "127.0.0.1  localhost"
-    echo "127.0.1.1  $HOSTNAME.$DOMAIN	$HOSTNAME"
-    echo "::1  ip6-localhost ip6-loopback"
-    echo "ff02::1  ip6-allnodes"
-    echo "ff02::2  ip6-allrouters"
-} >/etc/hosts
-
-# Configure /etc/xdg/user-dirs.defaults
-sed -i 's/^TEMPLATES=.*/TEMPLATES=Documents\/Templates/;s/^PUBLICSHARE=.*/PUBLICSHARE=Documents\/Public/;s/^DESKTOP=.*/DESKTOP=Documents\/Desktop/;s/^MUSIC=.*/MUSIC=Documents\/Music/;s/^PICTURES=.*/PICTURES=Documents\/Pictures/;s/^VIDEOS=.*/VIDEOS=Documents\/Videos/' /etc/xdg/user-dirs.defaults
-
-# Configure /etc/mdadm.conf
-mdadm --detail --scan >>/etc/mdadm.conf
-
-# Configure /etc/usbguard/usbguard-daemon.conf and /etc/usbguard/rules.conf
-usbguard generate-policy >/etc/usbguard/rules.conf
-usbguard add-user -g usbguard --devices=modify,list,listen --policy=list --exceptions=listen
-
-# Configure /etc/pam.d/system-login, /etc/security/faillock.conf, /etc/pam.d/su and /etc/pam.d/su-l
-echo "auth optional pam_faildelay.so delay=8000000" >>/etc/pam.d/system-login
-sed -i 's/^#.*dir.*=.*/dir = \/var\/lib\/faillock/' /etc/security/faillock.conf
-echo "auth required pam_wheel.so use_uid" >>/etc/pam.d/su
-echo "auth required pam_wheel.so use_uid" >>/etc/pam.d/su-l
-
-# Configure /etc/audit/auditd.conf
-sed -i 's/^log_group.*=.*/log_group = audit/' /etc/audit/auditd.conf
-
-# Configure mDNS for Avahi
-## Configure mDNS in /etc/systemd/resolved.conf
-sed -i 's/^#MulticastDNS=.*/MulticastDNS=no/' /etc/systemd/resolved.conf
-## Configure mDNS in /etc/nsswitch.conf
-sed -i 's/^hosts: mymachines/hosts: mymachines mdns_minimal [NOTFOUND=return]/' /etc/nsswitch.conf
+# Set default java
+archlinux-java set java-17-openjdk
 
 # Enable systemd services
 pacman -Qq "apparmor" &&
@@ -337,27 +350,8 @@ pacman -Qq "reflector" &&
 pacman -Qq "usbguard" &&
     systemctl enable usbguard.service
 
-# Add key for /dev/mapper/md0_crypt
-mkdir -p /etc/luks/keys
-dd bs=1024 count=4 if=/dev/urandom of=/etc/luks/keys/md0_crypt.key iflag=fullblock
-chmod 000 /etc/luks/keys/md0_crypt.key
-echo "Enter passphrase for /dev/md/md0"
-cryptsetup -v luksAddKey /dev/disk/by-uuid/"$MD0UUID" /etc/luks/keys/md0_crypt.key
-
-# Configure /etc/mkinitcpio.conf
-sed -i 's/^FILES=.*/FILES=(\/etc\/luks\/keys\/md0_crypt.key)/;s/^MODULES=.*/MODULES=(btrfs)/;s/^HOOKS=.*/HOOKS=(base udev autodetect keyboard keymap consolefont modconf block mdadm_udev encrypt filesystems fsck)/' /etc/mkinitcpio.conf
-## If on nvidia add nvidia nvidia_modeset nvidia_uvm nvidia_drm
-pacman -Qq "nvidia-dkms" &&
-    sed -i '/^MODULES=.*/s/)$/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+# Setup /boot and /efi
 mkinitcpio -P
-
-# Configure /etc/default/grub
-sed -i "s/^#GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/;s/^#GRUB_TERMINAL_OUTPUT=.*/GRUB_TERMINAL_OUTPUT=\"gfxterm\"/;s/^GRUB_GFXPAYLOAD_LINUX=.*/GRUB_GFXPAYLOAD_LINUX=keep/;s/^GRUB_GFXMODE=.*/GRUB_GFXMODE=""$GRUBRESOLUTION""x32,auto/;s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet loglevel=3 audit=1 lsm=landlock,lockdown,yama,integrity,apparmor,bpf iommu=pt zswap.enabled=0 cryptdevice=UUID=$MD0UUID:md0_crypt cryptkey=rootfs:\/etc\/luks\/keys\/md0_crypt.key cryptdevice=UUID=$MD1UUID:md1_crypt root=\/dev\/mapper\/md1_crypt\"/;s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"quiet loglevel=3 audit=1 lsm=landlock,lockdown,yama,integrity,apparmor,bpf iommu=pt zswap.enabled=0 cryptdevice=UUID=$MD0UUID:md0_crypt cryptkey=rootfs:\/etc\/luks\/keys\/md0_crypt.key cryptdevice=UUID=$MD1UUID:md1_crypt root=\/dev\/mapper\/md1_crypt\"/;s/^#GRUB_DISABLE_SUBMENU=.*/GRUB_DISABLE_SUBMENU=y/;s/^GRUB_DEFAULT=.*/GRUB_DEFAULT=0/;s/^#GRUB_SAVEDEFAULT=.*/GRUB_SAVEDEFAULT=false/" /etc/default/grub
-## If on nvidia add nvidia_drm.modeset=1 and set linux as default kernel
-pacman -Qq "nvidia-dkms" &&
-    sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=.*/s/"$/ nvidia_drm.modeset=1"/;/^GRUB_CMDLINE_LINUX=.*/s/"$/ nvidia_drm.modeset=1"/' /etc/default/grub
-pacman -Qq "intel-ucode" &&
-    sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=.*/s/"$/ intel_iommu=on"/;/^GRUB_CMDLINE_LINUX=.*/s/"$/ intel_iommu=on"/' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
