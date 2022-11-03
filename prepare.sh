d5ca82efe415aa2f24863ff43da3ead1aec1de4b
Leopold Johannes Meinel <leo@meinel.dev>
2022-11-03 12:39:56 +0100

    Set fmask and dmask for /mnt/share

--- prepare.sh
+++ prepare.sh
@@ -176,7 +176,7 @@ mount -o noexec,nodev,nosuid,noatime,space_cache=v2,compress=zstd,ssd,discard=as
 mount -o noexec,nodev,nosuid,noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=260 /dev/mapper/md1_crypt /mnt/var/lib/mysql
 mount -o noexec,nodev,nosuid,noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=261 /dev/mapper/md1_crypt /mnt/var/log
 mount -o nodev,nosuid,noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=262 /dev/mapper/md1_crypt /mnt/home
-mount -o noexec,nodev,noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=263 /dev/mapper/md1_crypt /mnt/share
+mount -o noexec,nodev,noatime,fmask=0117,dmask=0007,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=263 /dev/mapper/md1_crypt /mnt/share
 mount -o noexec,nodev,nosuid,noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=264 /dev/mapper/md1_crypt /mnt/.snapshots
 mount -o noexec,nodev,nosuid "$DISK1P1" /mnt/efi
 mount -o noexec,nodev,nosuid "$DISK2P1" /mnt/.efi.bak
