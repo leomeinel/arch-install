2de8bcd5d583376006482452e34d75fe5245fa82
Leopold Johannes Meinel <leo@meinel.dev>
2022-11-03 17:20:34 +0100

    Use normal setup instead of setup-guest

--- post.sh
+++ post.sh
@@ -23,7 +23,7 @@ GUESTUSER="<INSERT_GUESTUSER>"
 /dot-files.sh setup
 doas su -lc '/dot-files.sh setup' "$VIRTUSER"
 doas su -lc '/dot-files.sh setup' "$HOMEUSER"
-doas su -lc "/dot-files.sh setup-guest $GUESTUSER" "$GUESTUSER"
+doas su -lc '/dot-files.sh setup' "$GUESTUSER"
 doas su -lc "/dot-files.sh setup-root" root
 
 # Configure clock
