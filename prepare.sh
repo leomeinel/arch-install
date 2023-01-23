1ccbe2a449b00fbad05e6e2ad28879170422fc18
Leopold Johannes Meinel <leo@meinel.dev>
2023-01-23 03:18:49 +0100

    Reorder erasure of crypt volumes
    - This should prevent potential misdetection and unnecessary errors

--- prepare.sh
+++ prepare.sh
@@ -20,7 +20,7 @@ mountpoint -q /mnt &&
     umount -AR /mnt
 
 # Prompt user for disk
-# I will use this on an external SSD, therefore USB volumes will be listed too
+# I will use this on an external SSD, therefore USB volumes will be valid
 lsblk -drnpo SIZE,NAME -I 259,8,254
 read -rp "Which disk do you want to erase? (Type '/dev/sdX' fex.): " choice
 if lsblk -drnpo SIZE,NAME -I 259,8,254 $choice; then
@@ -31,30 +31,30 @@ else
     exit 125
 fi
 
-# Detect & close old crypt volumes
-if lsblk -rno TYPE | grep -q "crypt"; then
+# Detect, close & erase old crypt volumes
+if lsblk -rno TYPE "$DISK1" | grep -q "crypt"; then
     OLD_CRYPT_0="$(lsblk -Mrno TYPE,NAME $DISK1 | grep "crypt" | sed 's/crypt//' | sed -n '1p' | tr -d "[:space:]")"
     OLD_CRYPT_1="$(lsblk -Mrno TYPE,NAME $DISK1 | grep "crypt" | sed 's/crypt//' | sed -n '2p' | tr -d "[:space:]")"
+    OLD_DISK1P2="$(lsblk -rnpo TYPE,NAME $DISK1 | grep "part" | sed 's/part//' | sed -n '2p' | tr -d "[:space:]")"
+    OLD_DISK1P3="$(lsblk -rnpo TYPE,NAME $DISK1 | grep "part" | sed 's/part//' | sed -n '3p' | tr -d "[:space:]")"
+    ## Close old crypt volumes
     cryptsetup close "$OLD_CRYPT_0"
     cryptsetup close "$OLD_CRYPT_1"
-fi
-
-# Detect & erase old crypt volumes
-DISK1P2="$(lsblk -rnpo TYPE,NAME $DISK1 | grep "part" | sed 's/part//' | sed -n '2p' | tr -d "[:space:]")"
-if cryptsetup isLuks "$DISK1P2"; then
-    cryptsetup erase "$DISK1P2"
-    sgdisk -Z "$DISK1P2"
-else
-    echo "ERROR: Can't erase old crypt volume"
-    exit 125
-fi
-DISK1P3="$(lsblk -rnpo TYPE,NAME $DISK1 | grep "part" | sed 's/part//' | sed -n '3p' | tr -d "[:space:]")"
-if cryptsetup isLuks "$DISK1P3"; then
-    cryptsetup erase "$DISK1P3"
-    sgdisk -Z "$DISK1P3"
-else
-    echo "ERROR: Can't erase old crypt volume"
-    exit 125
+    ## Erase old crypt volumes
+    if cryptsetup isLuks "$OLD_DISK1P2"; then
+        cryptsetup erase "$OLD_DISK1P2"
+        sgdisk -Z "$OLD_DISK1P2"
+    else
+        echo "ERROR: Can't erase old crypt volume"
+        exit 125
+    fi
+    if cryptsetup isLuks "$OLD_DISK1P3"; then
+        cryptsetup erase "$OLD_DISK1P3"
+        sgdisk -Z "$OLD_DISK1P3"
+    else
+        echo "ERROR: Can't erase old crypt volume"
+        exit 125
+    fi
 fi
 
 # Load $KEYMAP & set time
