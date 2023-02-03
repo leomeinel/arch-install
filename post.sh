5b719286e416ae1a060edc69381e0ed7b4cc3887
Leopold Johannes Meinel <leo@meinel.dev>
2023-02-03 22:50:40 +0100

    Use/efi for determining DISK1

--- post.sh
+++ post.sh
@@ -378,7 +378,7 @@ pacman -Qq "usbguard-notifier" &&
 
 # Setup /boot & /efi
 doas mkinitcpio -P
-DISK1="$(lsblk -npo PKNAME $(findmnt -no SOURCE --target /boot) | tr -d "[:space:]")"
+DISK1="$(lsblk -npo PKNAME $(findmnt -no SOURCE --target /efi) | tr -d "[:space:]")"
 if udevadm info -q property --property=ID_BUS --value "$DISK1" | grep -q "usb"; then
     doas grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="grub-arch-games" --removable
 else
--- setup.sh
+++ setup.sh
@@ -196,7 +196,7 @@ chmod 777 /dot-files.sh
 
 # Configure /etc
 ## Configure /etc/crypttab
-DISK1="$(lsblk -npo PKNAME $(findmnt -no SOURCE --target /boot) | tr -d "[:space:]")"
+DISK1="$(lsblk -npo PKNAME $(findmnt -no SOURCE --target /efi) | tr -d "[:space:]")"
 DISK1P2="$(lsblk -rnpo TYPE,NAME "$DISK1" | grep "part" | sed 's/part//' | sed -n '2p' | tr -d "[:space:]")"
 DISK1P3="$(lsblk -rnpo TYPE,NAME "$DISK1" | grep "part" | sed 's/part//' | sed -n '3p' | tr -d "[:space:]")"
 MD0UUID="$(blkid -s UUID -o value $DISK1P2)"
