cb67e6b149513f94169b1df06cc57d3da2b464ef
Leopold Johannes Meinel <leo@meinel.dev>
2023-04-24 00:41:19 +0200

    Add missing disk detection

--- 0-erase-disks-before-v3.sh
+++ 0-erase-disks-before-v3.sh
@@ -12,6 +12,48 @@
 # Fail on error
 set -eu
 
+# Unmount everything from /mnt
+mountpoint -q /mnt &&
+    umount -AR /mnt
+
+# Detect disks
+readarray -t DISKS < <(lsblk -drnpo NAME -I 259,8,254 | tr -d "[:blank:]")
+DISKS_LENGTH="${#DISKS[@]}"
+for ((i = 0; i < DISKS_LENGTH; i++)); do
+    udevadm info -q property --property=ID_BUS --value "${DISKS[$i]}" | grep -q "usb" &&
+        {
+            unset 'DISKS[$i]'
+            continue
+        }
+    DISKS=("${DISKS[@]}")
+done
+[[ "${#DISKS[@]}" -ne 2 ]] &&
+    {
+        echo "ERROR: There are not exactly 2 disks attached!"
+        exit 1
+    }
+SIZE1="$(lsblk -drno SIZE "${DISKS[0]}" | tr -d "[:space:]")"
+SIZE2="$(lsblk -drno SIZE "${DISKS[1]}" | tr -d "[:space:]")"
+if [[ "$SIZE1" = "$SIZE2" ]]; then
+    DISK1="${DISKS[0]}"
+    DISK2="${DISKS[1]}"
+else
+    echo "ERROR: The attached disks don't have the same size!"
+    exit 1
+fi
+
+# Prompt user
+read -rp "Erase $DISK1 and $DISK2? (Type 'yes' in capital letters): " choice
+case "$choice" in
+YES)
+    echo "Erasing $DISK1 and $DISK2..."
+    ;;
+*)
+    echo "ERROR: User aborted erasing $DISK1 and $DISK2"
+    exit 1
+    ;;
+esac
+
 # Detect & close old crypt volumes
 if lsblk -rno TYPE | grep -q "crypt"; then
     OLD_CRYPT_0="$(lsblk -Mrno TYPE,NAME | grep "crypt" | sed 's/crypt//' | sed -n '1p' | tr -d "[:space:]")"
