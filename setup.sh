397cb6dac8d396b2f8b7de9220a2c40c8fbade55
Leopold Johannes Meinel <leo@meinel.dev>
2023-04-28 05:53:54 +0200

    Adjust for server

--- setup.sh
+++ setup.sh
@@ -153,20 +153,10 @@ reflector --save /etc/pacman.d/mirrorlist --country "$MIRRORCOUNTRIES" --protoco
 
 # Install packages
 pacman -Syu --noprogressbar --noconfirm --needed - <"$SCRIPT_DIR/pkgs-setup.txt"
-pacman -Qq "system-config-printer" >/dev/null 2>&1 &&
-    DEPENDENCIES+=$'cups-pk-helper'
-pacman -Qq "libvirt" >/dev/null 2>&1 &&
-    DEPENDENCIES+=$'\ndnsmasq'
-pacman -Qq "thunar" >/dev/null 2>&1 &&
-    DEPENDENCIES+=$'\ngvfs\nthunar-archive-plugin\nthunar-media-tags-plugin\nthunar-volman\ntumbler'
-pacman -Qq "wl-clipboard" >/dev/null 2>&1 &&
-    DEPENDENCIES+=$'\nmailcap'
-pacman -Qq "pipewire" >/dev/null 2>&1 &&
-    DEPENDENCIES+=$'\npipewire-alsa\npipewire-jack\npipewire-pulse'
 pacman -Qq "apparmor" >/dev/null 2>&1 &&
     DEPENDENCIES+=$'\npython-notify2'
-pacman -Qq "wlroots" >/dev/null 2>&1 &&
-    DEPENDENCIES+=$'\nxorg-xwayland'
+pacman -Qq "docker" >/dev/null 2>&1 &&
+    DEPENDENCIES+=$'\ndocker-scan'
 pacman -Syu --noprogressbar --noconfirm --needed --asdeps - <<<"$DEPENDENCIES"
 
 # Configure $SYSUSER
