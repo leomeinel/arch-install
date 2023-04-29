6a3a09fe673ffe6a1e049d4c7df42c0263f8f2ad
Leopold Johannes Meinel <leo@meinel.dev>
2023-04-29 04:30:46 +0200

    Use simple but effective hook instead
    - This regenerates all images
    - May be a bit unnecessary but it's the easiest approach and just uses the parameters already configured for dracut

--- etc/pacman.d/hooks/scripts/90-dracut-uefi-install.sh
+++ etc/pacman.d/hooks/scripts/90-dracut-uefi-install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+###
+# File: 90-dracut-uefi-install.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2023 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
 
+set -e
+if /usr/bin/mountpoint -q /efi; then
+    /usr/bin/umount -AR /efi
+fi
+/usr/bin/mount /efi
+/usr/bin/dracut --regenerate-all -fq
