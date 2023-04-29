d0a3517e63afa112d06150dc69be3b3cc60aa651
Leopold Johannes Meinel <leo@meinel.dev>
2023-04-29 04:35:40 +0200

    Use simple but effective hook instead
    - This deletes the contents of /efi/EFI/Linux which prevents unnecessary images from being there

--- etc/pacman.d/hooks/scripts/60-dracut-uefi-remove.sh
+++ etc/pacman.d/hooks/scripts/60-dracut-uefi-remove.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+###
+# File: 60-dracut-uefi-remove.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2023 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
 
+set -e
+if /usr/bin/mountpoint -q /efi; then
+    /usr/bin/umount -AR /efi
+fi
+/usr/bin/mount /efi
+shopt -s nocaseglob
+/usr/bin/rm -f /efi/EFI/Linux/*.efi
