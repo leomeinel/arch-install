5dc55f1ae6bff1b499434b0f06ebfd49a3635f23
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-07 23:21:22 +0200

    Log package version and date+time logged in UTC
    - This is essential to supply chain security

--- etc/pacman.d/hooks/scripts/50-pkglists.sh
+++ etc/pacman.d/hooks/scripts/50-pkglists.sh
@@ -3,15 +3,31 @@
 # File: 50-pkglists.sh
 # Author: Leopold Meinel (leo@meinel.dev)
 # -----
-# Copyright (c) 2022 Leopold Meinel & contributors
+# Copyright (c) 2023 Leopold Meinel & contributors
 # SPDX ID: GPL-3.0-or-later
 # URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
 # -----
 ###
 
-/usr/bin/pacman -Qqen >/var/log/pkglist-explicit.pacman.log
-/usr/bin/chmod 644 /var/log/pkglist-explicit.pacman.log
-/usr/bin/pacman -Qqem >/var/log/pkglist-foreign.pacman.log
-/usr/bin/chmod 644 /var/log/pkglist-foreign.pacman.log
-/usr/bin/pacman -Qqd >/var/log/pkglist-deps.pacman.log
-/usr/bin/chmod 644 /var/log/pkglist-deps.pacman.log
+{
+    /usr/bin/echo ''
+    /usr/bin/echo '########################'
+    /usr/bin/date -u +"%Y-%m-%dT%H:%M:%S %Z"
+    /usr/bin/pacman -Qen
+    /usr/bin/echo '########################'
+} >>/var/log/pkglist-explicit.pacman.log
+{
+    /usr/bin/echo ''
+    /usr/bin/echo '########################'
+    /usr/bin/date -u +"%Y-%m-%dT%H:%M:%S %Z"
+    /usr/bin/pacman -Qem
+    /usr/bin/echo '########################'
+} >>/var/log/pkglist-foreign.pacman.log
+{
+    /usr/bin/echo ''
+    /usr/bin/echo '########################'
+    /usr/bin/date -u +"%Y-%m-%dT%H:%M:%S %Z"
+    /usr/bin/pacman -Qd
+    /usr/bin/echo '########################'
+} >>/var/log/pkglist-deps.pacman.log
+/usr/bin/chmod 600 /var/log/pkglist-*.log
