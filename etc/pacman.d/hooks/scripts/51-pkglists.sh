855a29e966eb3de25f37a5932d94ede364fe0e1f
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-08 03:34:37 +0200

    Use function and change formatting

--- etc/pacman.d/hooks/scripts/51-pkglists.sh
+++ etc/pacman.d/hooks/scripts/51-pkglists.sh
@@ -9,25 +9,22 @@
 # -----
 ###
 
-{
+print_header() {
     /usr/bin/echo ''
     /usr/bin/echo '########################'
-    /usr/bin/date -u +"%Y-%m-%dT%H:%M:%S %Z"
-    /usr/bin/pacman -Qen
+    /usr/bin/date -u +"#%Y-%m-%dT%H:%M:%S %Z"
     /usr/bin/echo '########################'
+}
+{
+    print_header
+    /usr/bin/pacman -Qen
 } >>/var/log/pkglist-explicit.pacman.log
 {
-    /usr/bin/echo ''
-    /usr/bin/echo '########################'
-    /usr/bin/date -u +"%Y-%m-%dT%H:%M:%S %Z"
+    print_header
     /usr/bin/pacman -Qem
-    /usr/bin/echo '########################'
 } >>/var/log/pkglist-foreign.pacman.log
 {
-    /usr/bin/echo ''
-    /usr/bin/echo '########################'
-    /usr/bin/date -u +"%Y-%m-%dT%H:%M:%S %Z"
+    print_header
     /usr/bin/pacman -Qd
-    /usr/bin/echo '########################'
 } >>/var/log/pkglist-deps.pacman.log
 /usr/bin/chmod 644 /var/log/pkglist-*.log
