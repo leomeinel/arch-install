75251c81b57e020237214267a3321f9851b36bdd
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-09 05:23:20 +0200

    Configure anacron user jobs

--- sysuser.sh
+++ sysuser.sh
@@ -24,5 +24,27 @@ chmod +x ~/post.sh
 # Create backups directory
 mkdir -p ~/backups
 
+# Configure anacron user jobs
+mkdir -p ~/.anacron/{etc,spool,etc/cron.daily,etc/cron.weekly,etc/cron.monthly}
+{
+    echo '# /etc/anacrontab: configuration file for anacron'
+    echo ''
+    echo '# See anacron(8) and anacrontab(5) for details.'
+    echo ''
+    echo 'SHELL=/bin/sh'
+    echo 'PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
+    echo "MAILTO=$(whoami)"
+    echo '# the maximal random delay added to the base delay of the jobs'
+    echo 'RANDOM_DELAY=45'
+    echo '# the jobs will be started during the following hours only'
+    echo 'START_HOURS_RANGE=3-22'
+    echo ''
+    echo '#period in days   delay in minutes   job-identifier   command'
+    echo '1 5 cron.daily nice run-parts $HOME/.anacron/etc/cron.daily'
+    echo '7 25 cron.weekly nice run-parts $HOME/.anacron/etc/cron.weekly'
+    echo '@monthly 45 cron.monthly nice run-parts $HOME/.anacron/etc/cron.monthly'
+} >~/.anacron/anacrontab
+(crontab -l 2>/dev/null; echo '@hourly /usr/bin/anacron -s -t $HOME/.anacron/etc/anacrontab -S $HOME/.anacron/spool')| crontab -
+
 # Remove repo
 rm -rf ~/git
