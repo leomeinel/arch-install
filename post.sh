d3bc706f3732b2480bdc0f7375a5ef5e3118ce04
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-20 14:54:22 +0200

    Initialzize firefox for $SYSUSER

--- post.sh
+++ post.sh
@@ -28,6 +28,7 @@ doas localectl --no-convert set-keymap "$KEYMAP"
 doas localectl --no-convert set-x11-keymap "$KEYLAYOUT"
 
 # Initialize Firefox
+timeout 5 firefox --headless
 doas su -c 'timeout 5 firefox --headless' "$VIRTUSER"
 doas su -c 'timeout 5 firefox --headless' "$HOMEUSER"
 doas su -c 'timeout 5 firefox --headless' "$GUESTUSER"
