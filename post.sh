bab155c653e230db5b404167da8746a8225ec4cb
Leopold Johannes Meinel <leo@meinel.dev>
2023-05-20 14:58:30 +0200

    MERGE: Don't initialize firefox for $VIRTUSER

--- post.sh
+++ post.sh
@@ -29,7 +29,6 @@ doas localectl --no-convert set-x11-keymap "$KEYLAYOUT"
 
 # Initialize Firefox
 timeout 5 firefox --headless
-doas su -c 'timeout 5 firefox --headless' "$VIRTUSER"
 doas su -c 'timeout 5 firefox --headless' "$HOMEUSER"
 doas su -c 'timeout 5 firefox --headless' "$GUESTUSER"
 
