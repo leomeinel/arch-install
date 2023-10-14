073120e773671e20ab3e82a02ad992477682c3b6
leo <leo@meinel.dev>
2023-10-14 12:11:55 +0000

    Remove /dot-files.sh

--- post.sh
+++ post.sh
@@ -328,7 +328,7 @@ pacman -Qq "usbguard-notifier" >/dev/null 2>&1 &&
 rm -rf ~/git
 
 # Remove scripts
-
+doas rm -f /dot-files.sh
 doas rm -f /root/.bash_history
 rm -f ~/.bash_history
 rm -f "$SCRIPT_DIR/pkgs-post.txt"
