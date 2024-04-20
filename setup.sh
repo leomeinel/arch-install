64479523fcc194daa49ebc9f2fc793fae49827fd
leo <leo@meinel.dev>
2024-04-20 03:15:13 +0000

    chmod nitrokey-app

--- setup.sh
+++ setup.sh
@@ -425,6 +425,7 @@ ln -s "$(which nvim)" /usr/local/bin/vim
 chmod 755 /usr/local/bin/edit
 chmod 755 /usr/local/bin/ex
 chmod 755 /usr/local/bin/freetube
+chmod 755 /usr/local/bin/nitrokey-app
 chmod 755 /usr/local/bin/prismlauncher
 chmod 755 /usr/local/bin/sway-logout
 chmod 755 /usr/local/bin/trilium
