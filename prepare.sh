94ce18cab972c1beb134eb3eb5b16ddbdaf86261
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-11 18:22:24 +0100

    Add || true to dd statements

--- prepare.sh
+++ prepare.sh
@@ -115,24 +115,24 @@ vgchange -an || true
 ## Use dd and sgdisk -o to wipe the header and more to make sure that it is erased
 sgdisk -o "$DISK1" || true
 sgdisk -Z "$DISK1" || true
-dd if=/dev/zero of="$DISK1" bs=1M count=16384 status=progress
+dd if=/dev/zero of="$DISK1" bs=1M count=16384 status=progress || true
 
 if [[ -n "$DISK2" ]]; then
     sgdisk -o "$DISK2" || true
     sgdisk -Z "$DISK2" || true
-    dd if=/dev/zero of="$DISK2" bs=1M count=16384 status=progress
+    dd if=/dev/zero of="$DISK2" bs=1M count=16384 status=progress || true
 fi
 ## Prompt user if they want to secure wipe the whole disk
 if [[ -n "$DISK2" ]]; then
     read -rp "Secure wipe $DISK1 and $DISK2? (Type 'yes' in capital letters): " choice
     if [[ "$choice" == "YES" ]]; then
-        dd if=/dev/urandom of="$DISK1" bs="$(stat -c "%o" "$DISK1")" status=progress
-        dd if=/dev/urandom of="$DISK2" bs="$(stat -c "%o" "$DISK2")" status=progress
+        dd if=/dev/urandom of="$DISK1" bs="$(stat -c "%o" "$DISK1")" status=progress || true
+        dd if=/dev/urandom of="$DISK2" bs="$(stat -c "%o" "$DISK2")" status=progress || true
     fi
 else
     read -rp "Secure wipe $DISK1? (Type 'yes' in capital letters): " choice
     if [[ "$choice" == "YES" ]]; then
-        dd if=/dev/urandom of="$DISK1" bs="$(stat -c "%o" "$DISK1")" status=progress
+        dd if=/dev/urandom of="$DISK1" bs="$(stat -c "%o" "$DISK1")" status=progress || true
     fi
 fi
 
