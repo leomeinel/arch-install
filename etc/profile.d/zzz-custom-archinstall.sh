d55178321ce41a6dafccbb76e82591d900b5e25c
Leopold Johannes Meinel <leo@meinel.dev>
2025-03-13 15:16:48 +0100

    lynis hardening: Add TMOUT=3600

---
+++ etc/profile.d/zzz-custom-archinstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+###
+# File = zzz-custom-archinstall.sh
+# Author = Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2025 Leopold Meinel & contributors
+# SPDX ID = MIT
+# URL = https://opensource.org/licenses/MIT
+# -----
+###
+
+export TMOUT=3600
