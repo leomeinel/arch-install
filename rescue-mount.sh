909df2d6619cd23757f1e8071af438c39f96d617
Leopold Johannes Meinel <leo@meinel.dev>
2025-04-03 12:56:10 +0200

    Update headers

--- rescue-mount.sh
+++ rescue-mount.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 ###
-# File: rescue-mount.sh
-# Author: Leopold Meinel (leo@meinel.dev)
+# File = rescue-mount.sh
+# Author = Leopold Meinel (leo@meinel.dev)
 # -----
 # Copyright (c) 2025 Leopold Meinel & contributors
-# SPDX ID: MIT
-# URL: https://opensource.org/licenses/MIT
+# SPDX ID = MIT
+# URL = https://opensource.org/licenses/MIT
 # -----
 ###
 
