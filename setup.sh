61e4b5b5afe43f5264b5e7983a840d898b423568
Leopold Johannes Meinel <leo@meinel.dev>
2025-04-27 22:26:51 +0200

    Delete duplicate line

--- setup.sh
+++ setup.sh
@@ -709,7 +709,6 @@ FILES_755=(
     /usr/local/bin/tagger
     /usr/local/bin/upgrade-home
     /usr/local/bin/upgrade-packages
-    /usr/local/bin/upgrade-packages
     /usr/local/bin/wine
     /usr/local/bin/winetricks
 )
